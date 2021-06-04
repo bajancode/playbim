d7d010fdb5f166e0d752a696256f4eaea33ee07c
Bajan <valleyne+github@gmail.com>
2021-06-04 15:40:49 -0400

    ready to try deploy

--- models/campground.js
+++ models/campground.js
@@ -2,14 +2,27 @@ const mongoose  = require("mongoose");
 const Review = require("./review");
 const Schema = mongoose.Schema;
 
+const opts = { toJSON: {virtuals: true } };
+
 const CampgroundSchema = new Schema({
     title: String,
     images: [
-        { 
-            url: String,
-            filename: String
+        {
+        url: String,
+        filename: String
         }
     ],
+    geometry: {
+        type: {
+            type: String,
+            enum: ["Point"],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    },
     price: Number,
     description: String,
     location: String,
@@ -23,6 +36,11 @@ const CampgroundSchema = new Schema({
             ref: "Review"
         }
     ]
+}, opts);
+
+CampgroundSchema.virtual("properties.popUpMarkup").get(function () {
+    return `<strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
+    <p>${this.description.substring(0,20)}...</p>`
 })
 
 CampgroundSchema.post("findOneAndDelete", async function (doc) {
--- seeds/index.js
+++ seeds/index.js
@@ -19,15 +19,23 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async() => {
     await Campground.deleteMany({});
-    for(let i = 0; i < 50; i++) {
+    for(let i = 0; i < 300; i++) {
         const random1000 = Math.floor(Math.random() * 1000)
         const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground({
-            author: "60b00856fe5d022e4a0c75c5",
+            //My user ID
+            author: "60b00856fe5d022e4a0c75c5", 
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
             price,
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude
+                ]
+            },
             images: [
                 {
                   url: 'https://res.cloudinary.com/bugsy/image/upload/v1622571239/PlayBim/igzyyv0t9qqo7oblwxbn.jpg',
