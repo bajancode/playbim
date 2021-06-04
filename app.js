5b238a5d27f27176926a423df0ac6e4b4ae5b25f
Bajan <valleyne+github@gmail.com>
2021-06-04 15:48:22 -0400

    add start script

--- app.js
+++ app.js
@@ -162,6 +162,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error", {err})
 })
 
-app.listen(3000, () => {
-    console.log("Serving pun port 3000")
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Serving pun port ${port}`)
 })
